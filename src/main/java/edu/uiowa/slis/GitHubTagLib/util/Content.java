a72e84e2894d6f1220b9384814fd2c861130fe50
eichmann <david-eichmann@uiowa.edu>
2019-03-07 19:57:11 +0000

    Content retrieval and modification up and running.  The first example here is populating the roadmap repo README.md with the current project list.

--- src/main/java/edu/uiowa/slis/GitHubTagLib/util/Content.java
+++ src/main/java/edu/uiowa/slis/GitHubTagLib/util/Content.java
@@ -1,15 +1,18 @@
 package edu.uiowa.slis.GitHubTagLib.util;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Base64;
 import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
@@ -40,17 +43,33 @@ public class Content {
     static Logger logger = Logger.getLogger(Content.class);
     protected static LocalProperties prop_file = null;
 
+    static String client_id = "";
+    static String client_secret = "";
+
     static Decoder decoder = Base64.getMimeDecoder();
-    static Encoder encoder = Base64.getMimeEncoder();
+    static Encoder encoder = Base64.getMimeEncoder(-1, "\n".getBytes());
+    
+    static {
+	prop_file = PropertyLoader.loadProperties("github");
+	client_id = prop_file.getProperty("client_id");
+	client_secret = prop_file.getProperty("client_secret");	
+    }
 
     public static void main(String[] args) throws IOException {
-	switch (1) {
+	PropertyConfigurator.configure(args[0]);
+	switch (2) {
 	case 1:
 	    String content = read("data2health", "scits-platform", "README.md");
 	    logger.info("contents:\n" + content);
 	    break;
 	case 2:
-	    update("data2health", "scits-platform", "README.md", "TO DO", "workin' it");
+	    update("data2health", "scits-platform", "testing.md", "replacement", "workin' it", "trying an update");
+	    break;
+	case 3:
+	    write("data2health", "scits-platform", "testing.md", "TO DO", "workin' it");
+	    break;
+	case 4:
+	    replace("data2health", "scits-platform", "testing.md", "additional replacement content", "reworkin' it");
 	    break;
 	default:
 	    break;
@@ -70,18 +89,78 @@ public class Content {
 
     static String read(String owner, String repo, String path) throws IOException {
 	JSONObject response = readRaw(owner, repo, path);
-	logger.info("sha: " + response.getString("sha"));
+	logger.debug("sha: " + response.getString("sha"));
 
 	return new String(decoder.decode(response.getString("content").getBytes()));
     }
     
-    static void update(String owner, String repo, String path, String pattern, String replacement) throws IOException {
+    static String getSha(String owner, String repo, String path) throws IOException {
 	JSONObject response = readRaw(owner, repo, path);
-	String sha = response.getString("sha");
-	String content = new String(decoder.decode(response.getString("content").getBytes()));
-	logger.info("sha: " + sha);
+	return response.getString("sha");
+    }
+    
+    static void write(String owner, String repo, String path, String content, String message) throws IOException {
+	write(owner, repo, path, content, message, false);
+    }
+    
+    static void replace(String owner, String repo, String path, String content, String message) throws IOException {
+	write(owner, repo, path, content, message, true);
+    }
+    
+    static void write(String owner, String repo, String path, String content, String message, boolean rewrite) throws IOException {
+	StringBuffer buffer = new StringBuffer();
+	buffer.append("{\n");
+	buffer.append("  \"message\": \"" + quotify(message) + "\",\n");
+	if (rewrite)
+	    buffer.append("  \"sha\": \"" + getSha(owner, repo, path) + "\",\n");
+	buffer.append("  \"committer\": {\n");
+	buffer.append("     \"name\": \"David Eichmann\",\n");
+	buffer.append("     \"email\": \"david.eichmann@gmail.com\"\n");
+	buffer.append("   },\n");
+	buffer.append("  \"content\": \"" + encoder.encodeToString(content.getBytes()) + "\"\n");
+	buffer.append("}\n");
+	logger.debug("payload:\n" + buffer);
+	
+	submit("https://api.github.com/repos/" + owner + "/" + repo + "/contents/" + path, buffer.toString());
+    }
+    
+    static void update(String owner, String repo, String path, String pattern, String replacement, String message) throws IOException {
+	String content = read(owner, repo, path);
+	logger.debug("originalContent: " + content);
 	String newContent = content.replaceAll(pattern, replacement);
-	logger.info("newContent:\n" + newContent);
+	logger.debug("newContent:\n" + newContent);
+	replace(owner, repo, path, newContent, message);
     }
 
+    static private JSONObject submit(String url, String request) throws IOException {
+	// configure the connection
+	URL uri = new URL(url);
+	HttpURLConnection con = (HttpURLConnection) uri.openConnection();
+	con.setRequestMethod("PUT"); // type: POST, PUT, DELETE, GET
+	if (prop_file.getProperty("token") != null)
+	    con.setRequestProperty("Authorization", "token " + prop_file.getProperty("token"));
+	con.setRequestProperty("Accept","application/json");
+	con.setDoOutput(true);
+	con.setDoInput(true);
+	
+	// submit the GraphQL construct
+	logger.debug("request: " + request);
+	BufferedWriter out = new BufferedWriter(new OutputStreamWriter(con.getOutputStream()));
+	out.write(request);
+	out.flush();
+	out.close();
+
+	// pull down the response JSON
+	con.connect();
+	logger.info("response:" + con.getResponseCode());
+	BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+	JSONObject results = new JSONObject(new JSONTokener(in));
+	logger.debug("results:\n" + results.toString(3));
+	in.close();
+	return results;
+    }
+
+    static String quotify(String theString) {
+	return theString.replace("\"", "\\\"");
+    }
 }
---
+++ src/main/java/edu/uiowa/slis/GitHubTagLib/util/RoadmapMaintainer.java
@@ -0,0 +1,81 @@
+package edu.uiowa.slis.GitHubTagLib.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+public class RoadmapMaintainer {
+    static Logger logger = Logger.getLogger(JSONLoader.class);
+    protected static LocalProperties prop_file = null;
+    static Connection conn = null;
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+	boolean inPrefix = true;
+	boolean inList = true;
+	boolean databaseCalled = false;
+	prop_file = PropertyLoader.loadProperties("github");
+	getConnection();
+	
+	String originalContent = Content.read("data2health", "roadmap", "README.md");
+	StringBuffer newContent = new StringBuffer();
+	String buffer = null;
+	BufferedReader reader = new BufferedReader(new StringReader(originalContent));
+	while ((buffer = reader.readLine()) != null) {
+	    logger.info("buffer: " + buffer);
+	    if (inPrefix) {
+		newContent.append(buffer + "\n");
+		if (buffer.equals("# Phase 2 Projects"))
+		    inPrefix = false;
+	    }
+	    if (!inPrefix && inList) {
+		if (!buffer.equals("# Phase 2 Projects") && buffer.startsWith("# ")) {
+		    inList = false;
+		    newContent.append("\n");
+		}
+		if (!databaseCalled) {
+		    logger.info("calling database..,");
+		    // call the database for repo list
+		    injectProjects(newContent);
+		    databaseCalled = true;
+		}
+	    }
+	    if (!inPrefix && !inList) {
+		newContent.append(buffer + "\n");
+	    }
+	}
+	logger.info("final result:\n" + newContent);
+	Content.replace("data2health", "roadmap", "README.md", newContent.toString(), "refreshed by RoadmapMaintainer");
+    }
+    
+    static void injectProjects(StringBuffer buffer) throws SQLException {
+	PreparedStatement stmt = conn.prepareStatement("select repo_name,updated_title,actual_github_repo_link from dashboard.dashboard where actual_github_repo_link is not null order by lower(repo_name)");
+	ResultSet rs = stmt.executeQuery();
+	while (rs.next()) {
+	    String repo = rs.getString(1);
+	    String title = rs.getString(2);
+	    String url = rs.getString(3);
+	    buffer.append("## [" + repo + "](" + url + ")\n");
+	    buffer.append(title + "\n");
+	}
+	stmt.close();
+    }
+
+    static void getConnection() throws ClassNotFoundException, SQLException {
+	logger.info("connecting to database...");
+	Class.forName("org.postgresql.Driver");
+	Properties props = new Properties();
+	props.setProperty("user", prop_file.getProperty("jdbc.user"));
+	props.setProperty("password", prop_file.getProperty("jdbc.password"));
+	conn = DriverManager.getConnection("jdbc:postgresql://" + prop_file.getProperty("jdbc.host") + "/" + prop_file.getProperty("jdbc.database"), props);
+	conn.setAutoCommit(true);
+    }
+
+}
