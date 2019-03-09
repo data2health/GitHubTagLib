e5eb7ebb18ce68906396b094239a190f7ca7a173
eichmann <david-eichmann@uiowa.edu>
2019-03-09 20:44:24 +0000

    Added support to refresh the project list from the cd2hpm repo topic and set this to be the default.

--- src/main/java/edu/uiowa/slis/GitHubTagLib/util/RoadmapMaintainer.java
+++ src/main/java/edu/uiowa/slis/GitHubTagLib/util/RoadmapMaintainer.java
@@ -8,25 +8,29 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
 import org.cd2h.JSONTagLib.GraphQL.GitHubAPI;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class RoadmapMaintainer {
     static Logger logger = Logger.getLogger(RoadmapMaintainer.class);
     protected static LocalProperties prop_file = null;
     static Connection conn = null;
+    static boolean useAPI = true;
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
 	prop_file = PropertyLoader.loadProperties("github");
 	getConnection();
 	
-//	regenerateRoadMap();
-	GitHubAPI theAPI = new GitHubAPI();
-	JSONObject results =theAPI.submitSearch(theAPI.getStatement("projectDashboardsingle"));
-	logger.info("results:\n" + results.toString(3));
+	regenerateRoadmap();
     }
 	
     static void regenerateRoadmap() throws IOException, SQLException {
@@ -38,7 +42,7 @@ public class RoadmapMaintainer {
 	String buffer = null;
 	BufferedReader reader = new BufferedReader(new StringReader(originalContent));
 	while ((buffer = reader.readLine()) != null) {
-	    logger.info("buffer: " + buffer);
+	    logger.debug("buffer: " + buffer);
 	    if (inPrefix) {
 		newContent.append(buffer + "\n");
 		if (buffer.equals("# Phase 2 Projects"))
@@ -50,9 +54,12 @@ public class RoadmapMaintainer {
 		    newContent.append("\n");
 		}
 		if (!databaseCalled) {
-		    logger.info("calling database..,");
-		    // call the database for repo list
-		    injectProjects(newContent);
+		    logger.info("calling API/database..,");
+		    // call the API or the database for repo list
+		    if (useAPI)
+			injectProjectsFromAPI(newContent);
+		    else
+			injectProjectsFromDatabase(newContent);
 		    databaseCalled = true;
 		}
 	    }
@@ -60,11 +67,11 @@ public class RoadmapMaintainer {
 		newContent.append(buffer + "\n");
 	    }
 	}
-	logger.info("final result:\n" + newContent);
+	logger.debug("final result:\n" + newContent);
 	Content.replace("data2health", "roadmap", "README.md", newContent.toString(), "refreshed by RoadmapMaintainer");
     }
     
-    static void injectProjects(StringBuffer buffer) throws SQLException {
+    static void injectProjectsFromDatabase(StringBuffer buffer) throws SQLException {
 	PreparedStatement stmt = conn.prepareStatement("select repo_name,updated_title,actual_github_repo_link from dashboard.dashboard where actual_github_repo_link is not null order by lower(repo_name)");
 	ResultSet rs = stmt.executeQuery();
 	while (rs.next()) {
@@ -77,6 +84,19 @@ public class RoadmapMaintainer {
 	stmt.close();
     }
 
+    static void injectProjectsFromAPI(StringBuffer buffer) throws SQLException, IOException {
+	GitHubAPI theAPI = new GitHubAPI();
+	JSONObject results =theAPI.submitSearch(theAPI.getStatement("roadmapList"));
+	JSONArray array = results.getJSONObject("data").getJSONObject("search").getJSONArray("nodes");
+	sort(array);
+	logger.debug("result:\n" + array.toString(3));
+	for (int i = 0; i < array.length(); i++) {
+	    JSONObject repo = array.getJSONObject(i);
+	    buffer.append("## [" + repo.getString("name") + "](" + repo.getString("url") + ")\n");
+	    buffer.append(repo.optString("description") + "\n");
+	}
+    }
+
     static void getConnection() throws ClassNotFoundException, SQLException {
 	logger.info("connecting to database...");
 	Class.forName("org.postgresql.Driver");
@@ -85,6 +105,40 @@ public class RoadmapMaintainer {
 	props.setProperty("password", prop_file.getProperty("jdbc.password"));
 	conn = DriverManager.getConnection("jdbc:postgresql://" + prop_file.getProperty("jdbc.host") + "/" + prop_file.getProperty("jdbc.database"), props);
 	conn.setAutoCommit(true);
+    }
+    
+    static void sort(JSONArray jsonArr) {
+
+	List<JSONObject> jsonValues = new ArrayList<JSONObject>();
+	while (jsonArr.length() > 0)
+	    jsonValues.add((JSONObject)jsonArr.remove(0));
+
+	Collections.sort(jsonValues, new Comparator<JSONObject>() {
+	    // You can change "Name" with "ID" if you want to sort by ID
+	    private static final String KEY_NAME = "name";
+
+	    @Override
+	    public int compare(JSONObject a, JSONObject b) {
+		String valA = new String();
+		String valB = new String();
+
+		try {
+		    valA = (String) a.get(KEY_NAME);
+		    valB = (String) b.get(KEY_NAME);
+		} catch (JSONException e) {
+		    // do something
+		}
+
+		return valA.compareToIgnoreCase(valB);
+		// if you want to change the sort order, simply use the
+		// following:
+		// return -valA.compareTo(valB);
+	    }
+	});
+
+	for (int i = 0; i < jsonValues.size(); i++) {
+	    jsonArr.put(jsonValues.get(i));
+	}
     }
 
 }
